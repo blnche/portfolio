3b28c4ef2ad038249ecd8efb0be0b2a647bafe8b
blnche <peltierblanche@gmail.com>
2024-03-28 19:28:58 +0100

    working on hamburger nav

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    const toggleNav = () => {
+        const navButton = document.getElementById('navButton');
+        const nav = document.getElementById('nav');
+
+        navButton.addEventListener('click', () => {
+            if(navButton.classList.contains('close')) {
+                console.log('nav is close');
+                navButton.classList.add('open');
+                navButton.classList.remove('close');
+
+                nav.style.display = 'flex';
+
+            } else {
+                console.log('nav is open');
+                navButton.classList.add('close');
+                navButton.classList.remove('open');
+
+                nav.style.display = 'none';
+            }
+        })
+    }
+
+    toggleNav();
+})
