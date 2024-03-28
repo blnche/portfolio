c405f6fee6a02db39568af72df20f1f1eaca4f0b
blnche <peltierblanche@gmail.com>
2024-03-28 21:12:16 +0100

    revised js & fixed style, mobile is mostly done, to do text and images

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,36 @@
 window.addEventListener('DOMContentLoaded', () => {
 
+    const navButton = document.getElementById('navButton');
+    const nav = document.getElementById('nav');
+    const navLinks = document.querySelectorAll('nav li a');
+
     const toggleNav = () => {
-        const navButton = document.getElementById('navButton');
-        const nav = document.getElementById('nav');
 
         navButton.addEventListener('click', () => {
-            if(navButton.classList.contains('close')) {
-                console.log('nav is close');
-                navButton.classList.add('open');
-                navButton.classList.remove('close');
-
-                nav.style.display = 'block';
-
-            } else {
-                console.log('nav is open');
-                navButton.classList.add('close');
-                navButton.classList.remove('open');
-
-                nav.style.display = 'none';
-            }
+            navStatus();
         })
+
+        navLinks.forEach(link => link.addEventListener('click', () => {
+            navStatus();
+            })
+        );
+    }
+
+    const navStatus = () => {
+        
+        if(window.getComputedStyle(nav).display === 'none') {
+            navButton.classList.add('open');
+            navButton.classList.remove('close');
+
+            nav.style.display = 'block';
+
+        } else if (window.getComputedStyle(nav).display === 'block') {
+
+            navButton.classList.add('close');
+            navButton.classList.remove('open');
+
+            nav.style.display = 'none';
+        }
     }
 
     toggleNav();
