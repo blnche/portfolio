62827d34f924e29e0442f1c5326399f70e12f3c8
blnche <peltierblanche@gmail.com>
2024-05-17 19:24:07 +0200

    migrating to react

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import NavBar from "./NavBar"
+
+const Header = () => {
+    return (
+        <>
+        <header>
+
+            <h1>{'{Blanche}'}</h1>
+            <NavBar/>
+        </header>
+        </>
+    )
+}
+
+export default Header
