7687f895b480c9b72439a014db1a4a451f39c76c
blnche <peltierblanche@gmail.com>
2024-05-17 20:33:30 +0200

    abort server

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import SocialLinks from "./SocialLinks"
+
+const Footer = () => {
+    return (
+        <>
+            <footer>
+                <SocialLinks />
+                <p>Design et création par Blanche Peltier</p>
+            </footer>
+        </>
+    )
+}
+
+export default Footer
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,15 @@
+const NavBar = () => {
+    return (
+        <>
+            <nav>
+                <ul>
+                    <li><a href="">Formation</a></li>
+                    <li><a href="">Projets</a></li>
+                    <li><a href="">Contact</a></li>
+                </ul>
+            </nav>
+        </>
+    )
+}
+
+export default NavBar
